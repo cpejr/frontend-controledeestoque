6aaf62a882a290f449c2e4f70392936187e1961c
André Versiani <lumini2@hotmail.com.br>
2020-10-05 18:08:18 -0300

    pesquisa da lista de usuários completa

--- src/Pages/ListaDeUsuarios/ListaDeUsuarios.js
+++ src/Pages/ListaDeUsuarios/ListaDeUsuarios.js
@@ -1,129 +1,159 @@
-import React, {useState, useEffect} from 'react'
-import {useHistory} from 'react-router-dom'
-import styles from './ListaDeUsuariosStyle'
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import styles from "./ListaDeUsuariosStyle";
+import produtostyles from "../ListaDeProdutos/listaDeProdutosStyle";
 
-let users = [
-    {
-        userName: 'Arthur Lima',
-        userType: 'Manager'
-    },
-    {
-        userName: 'Izabela Brandt',
-        userType: 'Manager'
-    },
-    {
-        userName: 'João Trindade',
-        userType: 'Manager'
-    },
-    {
-        userName: 'Carol Batista',
-        userType: 'Manager'
-    },
-    {
-        userName: 'Raphael Souza',
-        userType: 'Admin'
-    },
-    {
-        userName: 'Elias Silva',
-        userType: 'Admin'
-    },
-    {
-        userName: 'Vinícius Morais',
-        userType: 'Admin'
-    },
-    {
-        userName: 'Lucas Milhorato',
-        userType: 'Employee'
-    },
-    {
-        userName: 'André Pinto',
-        userType: 'Employee'
-    },
-    {
-        userName: 'Bernardo Andrade',
-        userType: 'Employee'
-    },
-    {
-        userName: 'Lucas Chaia',
-        userType: 'Employee'
-    },
-]
+let USERS = [
+  {
+    userName: "Arthur Lima",
+    userType: "Manager",
+  },
+  {
+    userName: "Izabela Brandt",
+    userType: "Manager",
+  },
+  {
+    userName: "João Trindade",
+    userType: "Manager",
+  },
+  {
+    userName: "Carol Batista",
+    userType: "Manager",
+  },
+  {
+    userName: "Raphael Souza",
+    userType: "Admin",
+  },
+  {
+    userName: "Elias Silva",
+    userType: "Admin",
+  },
+  {
+    userName: "Vinícius Morais",
+    userType: "Admin",
+  },
+  {
+    userName: "Lucas Milhorato",
+    userType: "Employee",
+  },
+  {
+    userName: "André Pinto",
+    userType: "Employee",
+  },
+  {
+    userName: "Bernardo Andrade",
+    userType: "Employee",
+  },
+  {
+    userName: "Lucas Chaia",
+    userType: "Employee",
+  },
+];
 
+export default function ListaDeUsuarios() {
+  const history = useHistory();
+  const [search, setSearch] = useState("");
+  const [users, setUsers] = useState(USERS);
+  const filteredNames = users.filter((name) =>
+    name.userName.toLocaleLowerCase().includes(search.toLowerCase())
+  );
 
+  function isInFilteredNames(filteredNames, user) {
+    let pin = 0;
+    filteredNames.map((pessoa) => {
+      if (pessoa.userName == user) {
+        pin += 1;
+      }
+    });
+    if (pin > 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
+  console.log(filteredNames);
 
-export default function ListaDeUsuarios(){
-    const history = useHistory()
-    return(
-        <div style={{minHeight: '100%', display: 'flex', flexDirection: 'column'}}>
+  return (
+    <div
+      style={{ minHeight: "100%", display: "flex", flexDirection: "column" }}
+    >
+      <input
+        style={styles.inputField}
+        type="text"
+        placeholder="Pesquisar"
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-            <div style={styles.userTypeHeader}>ADMINS</div>
-            <div style={styles.userTypeContainer}>
-                {users.map(user=>{
-                    if(user.userType==='Admin'){
-                        return (
-                            <div style={styles.userContainer} 
-                                onClick={()=>{
-                                    history.push({
-                                        pathname: '/usuario',
-                                        state: {
-                                            calledRoute: 'usuario',
-                                            user: user
-                                        }
-                                    })}
-                                }
-                            >
-                                <div>{user.userName}</div>
-                            </div>
-                        )
-                    }
-                })}
-            </div>
+      <div style={styles.userTypeHeader}>ADMINS</div>
+      <div style={styles.userTypeContainer}>
+        {filteredNames.map((user) => {
+          if (user.userType === "Admin") {
+            return (
+              <div
+                style={styles.userContainer}
+                onClick={() => {
+                  history.push({
+                    pathname: "/usuario",
+                    state: {
+                      calledRoute: "usuario",
+                      user: user,
+                    },
+                  });
+                }}
+              >
+                <div>{user.userName}</div>
+              </div>
+            );
+          }
+        })}
+      </div>
 
-            <div style={styles.userTypeHeader}>GERENTES</div>
-            <div style={styles.userTypeContainer}>
-                {users.map(user=>{
-                    if (user.userType==='Manager'){
-                        return (
-                            <div style={styles.userContainer} 
-                                onClick={()=>{
-                                    history.push({
-                                        pathname: '/usuario',
-                                        state: {
-                                            user: user
-                                        }
-                                    })}
-                                }
-                            >
-                                <div>{user.userName}</div>
-                            </div>
-                        )
-                    }
-                })}
-            </div>
+      <div style={styles.userTypeHeader}>GERENTES</div>
+      <div style={styles.userTypeContainer}>
+        {filteredNames.map((user) => {
+          if (user.userType === "Manager") {
+            return (
+              <div
+                style={styles.userContainer}
+                onClick={() => {
+                  history.push({
+                    pathname: "/usuario",
+                    state: {
+                      user: user,
+                    },
+                  });
+                }}
+              >
+                <div>{user.userName}</div>
+              </div>
+            );
+          }
+        })}
+      </div>
 
-            <div style={styles.userTypeHeader}>EMPREGADOS</div>
-            <div style={styles.userTypeContainer}>
-                {users.map(user=>{
-                    if (user.userType==='Employee'){
-                        return (
-                            <div style={styles.userContainer} 
-                                onClick={()=>{
-                                    history.push({
-                                        pathname: '/usuario',
-                                        state: {
-                                            user: user
-                                        }
-                                    })}
-                                }
-                            >
-                                <div>{user.userName}</div>
-                            </div>
-                        )
-                    }
-                })}
-            </div>
-
-        </div>
-    )
+      <div style={styles.userTypeHeader}>EMPREGADOS</div>
+      <div style={styles.userTypeContainer}>
+        {filteredNames.map((user) => {
+          if (user.userType === "Employee") {
+            return (
+              <div
+                style={styles.userContainer}
+                onClick={() => {
+                  history.push({
+                    pathname: "/usuario",
+                    state: {
+                      user: user,
+                    },
+                  });
+                }}
+              >
+                <div>{user.userName}</div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 }
--- src/Pages/ListaDeUsuarios/ListaDeUsuariosStyle.js
+++ src/Pages/ListaDeUsuarios/ListaDeUsuariosStyle.js
@@ -1,37 +1,61 @@
+import {
+  fontSizePadrao,
+  azulPadrao,
+  fontCinzaEscuro,
+  brancoPadrao,
+  fontPadrao,
+} from "../../StylePadrao/stylePadrao";
 
-const styles ={
-    userTypeContainer:{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap',
-        minHeight: '25%'
-    },
-    userContainer: {
-        backgroundColor: '#ffff',
-        height: '15vh',
-        width: '18vh',
-        border: '1px solid #707070',
-        margin: '2vh',
-        marginBottom: '0px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#043741',
-        fontSize: '3vh',
-        textTransform: 'uppercase',
-        textAlign: 'center',
-        paddingLeft: '2vh',
-        paddingRight: '2vh',
-        cursor: 'pointer'
-    },
-    userTypeHeader: {
-        alignSelf: 'center',
-        fontSize: '3vh',
-        fontWeight: '600',
-        color: '#043741',
-        marginTop: '3vh',
-    }
-}
+const styles = {
+  userTypeContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap",
+    minHeight: "25%",
+  },
+  userContainer: {
+    backgroundColor: "#ffff",
+    height: "15vh",
+    width: "18vh",
+    border: "1px solid #707070",
+    margin: "2vh",
+    marginBottom: "0px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#043741",
+    fontSize: "3vh",
+    textTransform: "uppercase",
+    textAlign: "center",
+    paddingLeft: "2vh",
+    paddingRight: "2vh",
+    cursor: "pointer",
+  },
+  userTypeHeader: {
+    alignSelf: "center",
+    fontSize: "3vh",
+    fontWeight: "600",
+    color: "#043741",
+    marginTop: "3vh",
+  },
 
-export default styles
+  inputField: {
+    textAlign: "left",
+    borderRadious: "0px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "2%",
+    border: "1px solid #707070",
+    backgroundColor: brancoPadrao,
+    height: "12%",
+    width: "35%",
+    fontSize: "120%",
+    fontFamily: fontPadrao,
+    color: fontCinzaEscuro,
+    marginTop: "4%",
+    paddingLeft: "0.5%",
+  },
+};
+
+export default styles;
